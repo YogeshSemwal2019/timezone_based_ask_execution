f20fff9f572eaa34a15f9075e164c0a0dc739af3
Yogesh Semwal <yogeshsemwal403@gmail.com>
2020-10-13 20:43:24 +0530

    some fixes

--- timezone_based_ask_execution/task_execution.py
+++ timezone_based_ask_execution/task_execution.py
@@ -8,7 +8,7 @@ class TaskExecution():
     
     @staticmethod
     def execute(task_start_time,task_end_time,current_task_time,task,currentday=None):
-        if task_start_time < datetime.strptime(current_task_time,"%H:%M:%S").time() or task_end_time < datetime.strptime(current_task_time,"%H:%M:%S").time() and currentday is not None: 
+        if task_start_time > datetime.strptime(current_task_time,"%H:%M:%S").time() or task_end_time < datetime.strptime(current_task_time,"%H:%M:%S").time() and currentday is not None: 
             if task.weekdays is not None:
                if currentday not in task.weekdays.split(","):
                    nextday = currentday
@@ -19,7 +19,7 @@ class TaskExecution():
                        nextday = calendar.day_name[nextdate.weekday()]
                        #print(str(nextday) + "  " + str(task_start_time) )                       
                    return False,task_start_time,nextday
-            return True                  
+            return True,                  
         elif task_start_time < datetime.strptime(current_task_time,"%H:%M:%S").time() and task_end_time > datetime.strptime(current_task_time,"%H:%M:%S").time(): 
             if task.weekdays is not None:
                #print(task.weekdays)
@@ -31,6 +31,6 @@ class TaskExecution():
                        nextday = calendar.day_name[nextdate.weekday()]
                    #print(str(nextday) + "  " + str(task_start_time) )                       
                    return False,task_start_time,nextday
-            return True
+            return True,
         else:
             return False,task_start_time
